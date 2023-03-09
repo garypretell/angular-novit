4ca52763628b7e07774cb461cb291f4292e54fee
garypretell <garypretell24gmail.com>
2023-03-09 09:26:20 -0500

    modal close

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalService } from '@core/services/modal.service';
+import { MODAL_ALERT } from '@core/constants/modal-title';
 
 
 @Component({
@@ -15,11 +16,13 @@ export class ModalComponent   {
   constructor(public modalService: ModalService) { }
 
   showModal() {
-    this.modalService.modalData.show = true;
+    MODAL_ALERT.show = true;
+    this.modalService.modalData = MODAL_ALERT;
   }
 
   hideModal() {
-    this.modalService.modalData.show = false;
+    MODAL_ALERT.show = false;
+    this.modalService.modalData = MODAL_ALERT;
   }
 
 }
